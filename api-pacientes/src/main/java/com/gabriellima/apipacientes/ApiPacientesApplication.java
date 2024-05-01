4438f46ab4790f33015c119abf6096fdd54220a4
GabrielLimaDeSouza <gabriel.souza.1354648@sga.pucminas.br>
2024-05-01 10:44:04 -0300

    chore: initial config

---
+++ api-pacientes/src/main/java/com/gabriellima/apipacientes/ApiPacientesApplication.java
@@ -0,0 +1,13 @@
+package com.gabriellima.apipacientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPacientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPacientesApplication.class, args);
+	}
+
+}
